22edb7d73bc6409af673ccebb9128ef59ba18f79
FokusNIK9 <78025800+FokusNIK9@users.noreply.github.com>
2022-05-12 21:05:49 +0300

    Add files via upload

---
+++ Test_soft/Test_soft/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Test_soft;
+
+using Divine.Service;
+
+
+public class Class1 : Bootstrapper
+{
+    protected override void OnActivate()
+    {
+        System.Console.WriteLine("OnActivate()");
+    }
+
+    protected override void OnDeactivate()
+    { 
+        System.Console.WriteLine("OnDeactivate()");
+    }
+}
